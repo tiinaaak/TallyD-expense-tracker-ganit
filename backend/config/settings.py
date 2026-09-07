0f180812c6c5f8b22e8145adb155c6a66814179d
Tina Kashyap <tinakashyap2414@gmail.com>
2026-09-07 13:47:31 +0530

    Connect Django to PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,13 +95,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASE
 # ============================================================
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config('DB_ENGINE', default='sqlite') == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT', default='5432'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ============================================================
 # PASSWORD VALIDATION
